0c88d7f794a6125c6a0ba5542a0a2b2e2411d70a
aniket sharma <48768524+thedefiantone17@users.noreply.github.com>
2022-04-10 18:54:23 +0530

    first commit

---
+++ EmployeeService/src/main/resources/my.sql
@@ -0,0 +1,3 @@
+ insert into roles values('1','ADMIN');
+ INSERT INTO USERS VALUES('1','$2a$12$Flp9iE2bTAJLGI1AmPJtquzTFLaoVyTLKqr8OhRURZJignqQxuAHm','user1');
+ insert into users_roles values(1,1);
